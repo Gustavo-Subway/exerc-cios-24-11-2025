aef38778de561e6f98cab5dbabd1c36847d2327a
Gustavo-Subway <gustavosilvadias07@gmail.com>
2025-11-24 18:46:07 -0300

    Add files via upload

---
+++ exe1/exe1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letras = new char[10];
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite a {0}º letra para formar uma palavra: ", i + 1);
+                letras[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("A palavra é: ");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write(letras[i]);
+            }
+            Console.WriteLine();
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}° letra:{1} ", i + 1, letras[i]);
+            }
+        }
+    }
+}
