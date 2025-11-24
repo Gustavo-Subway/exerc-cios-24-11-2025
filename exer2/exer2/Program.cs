6131eab82eee515a757713e25a67efc657e3db1e
Gustavo-Subway <gustavosilvadias07@gmail.com>
2025-11-24 18:46:25 -0300

    Add files via upload

---
+++ exer2/exer2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, e;
+
+            Console.WriteLine("Digite quantas letras tem a palavra");
+            e = int.Parse(Console.ReadLine());
+
+            char[] letras = new char[e];
+
+            for (i = 0; i < e; i++)
+            {
+                Console.WriteLine("Digite a {0}º letra da palavra: ", i + 1);
+                letras[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("A palavra é: ");
+
+            for (i = 0; i < e; i++)
+            {
+                Console.Write(letras[i]);
+            }
+            Console.WriteLine();
+            for (i = 0; i < e; i++)
+            {
+                Console.WriteLine("{0}° letra:{1} ", i + 1, letras[i]);
+            }
+        }
+    }
+}
